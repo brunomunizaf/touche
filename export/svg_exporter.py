4b099580a46181bf4c36c31e04e0331f31e2ccad
Bruno Muniz Azevedo Filho <bruno@elixxir.io>
2025-06-17 22:47:32 -0300

    fix

--- export/svg_exporter.py
+++ export/svg_exporter.py
@@ -3,7 +3,9 @@ import svgwrite
 class SVGExporter:
     def __init__(self, width, height):
         # width and height in mm
-        self.dwg = svgwrite.Drawing(size=(f"{width}mm", f"{height}mm"))
+        self.dwg = svgwrite.Drawing(profile='full', size=(f"{width}mm", f"{height}mm"))
+        # Add viewBox to ensure proper scaling
+        self.dwg.attribs['viewBox'] = f"0 0 {width} {height}"
     def add_component(self, component, x_offset, y_offset):
         # Draw the component at the given offset
         component.draw(self.dwg, x_offset, y_offset)
