b3d209acd4c18188eadaa641bbdc57e55d1c3e87
Bruno Muniz Azevedo Filho <bruno@elixxir.io>
2025-08-22 19:37:36 -0300

    fix internal lining base loose top

--- export/components/internal_lining/internal_lining_base_loose_top.py
+++ export/components/internal_lining/internal_lining_base_loose_top.py
@@ -1,70 +1,77 @@
 class InternalLiningBaseForLooseTopComponent:
     def __init__(self, width_cm, height_cm, depth_cm, thickness_mm):
-        self.width = width_cm * 10  # convert to mm
-        self.height = height_cm * 10
-        self.depth = depth_cm * 10
-        self.thickness = thickness_mm
+        self.largura = width_cm * 10  # convert to mm
+        self.altura = height_cm * 10
+        self.profundidade = depth_cm * 10
+        self.espessura = thickness_mm
         self._compute_size()
 
     def _compute_size(self):
-        self._total_width = self.width + 2 * self.depth
-        self._total_height = self.height + 2 * self.depth
-
-    @property
-    def total_width(self):
-        return self._total_width
-
-    @property
-    def total_height(self):
-        return self._total_height
+        self.total_width = self.largura + 2 * self.profundidade
+        self.total_height = self.altura + 2 * self.profundidade
     
     def draw(self, dwg, x_offset, y_offset):
-        main_left_x = self.depth + x_offset
-        main_right_x = main_left_x + self.width
-        main_bottom_y = self.depth + y_offset
-        main_top_y = main_bottom_y + self.height
-        flap_left_x = (main_left_x - self.depth) + 0.5
-        flap_right_x = (main_right_x + self.depth) - 0.5
-        flap_bottom_y = (main_bottom_y - self.depth) + 0.5
-        flap_top_y = (main_top_y + self.depth) - 0.5
-        t = self.thickness
+        main_left_x = self.profundidade + x_offset
+        main_right_x = main_left_x + self.largura
+        main_bottom_y = self.profundidade + y_offset
+        main_top_y = main_bottom_y + self.altura
+        flap_left_x = (main_left_x - self.profundidade) + 0.5
+        flap_right_x = (main_right_x + self.profundidade) - 0.5
+        flap_bottom_y = (main_bottom_y - self.profundidade) + 0.5
+        flap_top_y = (main_top_y + self.profundidade) - 0.5
 
         path = dwg.path(stroke="navy", fill="none", stroke_width='0.1')
 
         path.push("M", main_left_x, main_top_y)
 
-        path.push("L", main_left_x - t, main_top_y, 
-                  main_left_x - t, main_top_y + self.depth / 2, 
-                  main_left_x, main_top_y + self.depth / 2, 
-                  main_left_x, flap_top_y,
-                  main_right_x, flap_top_y, 
-                  main_right_x, main_top_y + self.depth / 2, 
-                  main_right_x + t, main_top_y + self.depth / 2, 
-                  main_right_x + t, main_top_y, 
-                  main_right_x, main_top_y
-                  )
+        path.push("L", 
+            main_left_x - self.espessura, main_top_y, 
+            main_left_x - self.espessura, main_top_y + self.profundidade / 2, 
+            main_left_x, main_top_y + self.profundidade / 2, 
+            main_left_x, flap_top_y,
+            main_right_x, flap_top_y, 
+            main_right_x, main_top_y + self.profundidade / 2, 
+            main_right_x + self.espessura, main_top_y + self.profundidade / 2, 
+            main_right_x + self.espessura, main_top_y, 
+            main_right_x, main_top_y
+        )
 
         path.push("M", main_left_x, main_bottom_y)
 
-        path.push("L", main_left_x - t, main_bottom_y, 
-                  main_left_x - t, main_bottom_y - self.depth / 2, 
-                  main_left_x, main_bottom_y - self.depth / 2, 
-                  main_left_x, flap_bottom_y,
-                  main_right_x, flap_bottom_y, 
-                  main_right_x, main_bottom_y - self.depth / 2, 
-                  main_right_x + t, main_bottom_y - self.depth / 2, 
-                  main_right_x + t, main_bottom_y, 
-                  main_right_x, main_bottom_y
-                  )
+        path.push("L", 
+            main_left_x - self.espessura, main_bottom_y, 
+            main_left_x - self.espessura, main_bottom_y - self.profundidade / 2, 
+            main_left_x, main_bottom_y - self.profundidade / 2, 
+            main_left_x, flap_bottom_y,
+            main_right_x, flap_bottom_y, 
+            main_right_x, main_bottom_y - self.profundidade / 2, 
+            main_right_x + self.espessura, main_bottom_y - self.profundidade / 2, 
+            main_right_x + self.espessura, main_bottom_y, 
+            main_right_x, main_bottom_y
+        )
 
         path.push("M", main_left_x, main_bottom_y)
 
-        path.push("L", main_left_x - self.depth / 2, main_bottom_y, main_left_x - self.depth / 2, main_bottom_y - t, flap_left_x, main_bottom_y - t,
-                  flap_left_x, main_top_y + t, main_left_x - self.depth / 2, main_top_y + t, main_left_x - self.depth / 2, main_top_y, main_left_x, main_top_y)
+        path.push("L", 
+            main_left_x - self.profundidade / 2, main_bottom_y, 
+            main_left_x - self.profundidade / 2, main_bottom_y - self.espessura, 
+            flap_left_x, main_bottom_y - self.espessura,
+            flap_left_x, main_top_y + self.espessura, 
+            main_left_x - self.profundidade / 2, main_top_y + self.espessura, 
+            main_left_x - self.profundidade / 2, main_top_y, 
+            main_left_x, main_top_y
+        )
 
         path.push("M", main_right_x, main_bottom_y)
 
-        path.push("L", flap_right_x - self.depth / 2, main_bottom_y, flap_right_x - self.depth / 2, main_bottom_y - t, flap_right_x, main_bottom_y - t,
-                  flap_right_x, main_top_y + t, flap_right_x - self.depth / 2, main_top_y + t, flap_right_x - self.depth / 2, main_top_y, main_right_x, main_top_y)
+        path.push("L",
+            main_right_x + self.profundidade / 2, main_bottom_y,
+            main_right_x + self.profundidade / 2, main_bottom_y - self.espessura,
+            flap_right_x, main_bottom_y - self.espessura,
+            flap_right_x, main_top_y + self.espessura,
+            main_right_x + self.profundidade / 2, main_top_y + self.espessura,
+            main_right_x + self.profundidade / 2, main_top_y,
+            main_right_x, main_top_y
+        )
 
         dwg.add(path) 
